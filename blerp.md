3ba99861bebc9f9b76b6447b3a5c57d11d06cc61
s0crates <115264178+s0crates-eth@users.noreply.github.com>
2023-06-03 18:16:44 +0700

    contract test started // created notes

---
+++ blerp.md
@@ -0,0 +1,9 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-. */
+/* -.-.-.-.-.-.-.-. QUIZ 2 EARN v0.0.1 -.-.-.-.-.-.-.-. */
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-. */
+
+# TO DO
+- add a payment function to create passive revenue
+    - they provide payment and submit answer in same txn
+    - if correct answer then reward, if not then end
+- 
--- test/Q2E.t.sol
+++ test/Q2E.t.sol
@@ -4,21 +4,19 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import "../src/Q2E.sol";
 
-contract CounterTest is Test {
-    Counter public counter;
+contract Q2ETest is Test {
+    Q2E public q2e;
 
     function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
+        // create a question
+        string memory question = "2 * 2 = ";
+        string memory answer = "4";
+        bytes32 salt = bytes32("69420");
+        bytes32 hashedAnswer = keccak256(abi.encodePacked(salt, answer));
+        emit log_bytes32(hashedAnswer);
     }
 
-    function testIncrement() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
-    }
-
-    function testSetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+    function testtrue() public {
+        assertTrue(true);
     }
 }
