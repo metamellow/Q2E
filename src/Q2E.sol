7561ebd2ae0f6dbc4a736f90083b4687cdcfd88f
s0crates <115264178+s0crates-eth@users.noreply.github.com>
2023-06-15 15:56:56 +0700

    finishing up the auto swap stuff

--- src/Q2E.sol
+++ src/Q2E.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.13;
 /* -.-.-.-.-.-. BANK OF NOWHERE LOTTO v0.1 -.-.-.-.-.-. */
 /* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-. */
 
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Router.sol";
 
@@ -13,7 +14,7 @@ contract Q2E {
     bytes32 public hashedAnswer;
     string public question;
     address public erc20contract;
-    address public taxW;
+    address public erc20LP;
     uint256 public erc20Base;
     uint256 public erc20Fee;
     uint256 public counter;
@@ -26,14 +27,14 @@ contract Q2E {
         string memory _question, 
         bytes32 _hashedAnswer, 
         address _erc20contract, 
-        address _taxW, 
+        address _erc20LP;
         uint _erc20Base, 
         uint _erc20Fee
         ){
         question = _question; // "xx?"
         hashedAnswer = _hashedAnswer; // "xx"
-        erc20contract = _erc20contract; // "0x47E53f0Ddf71210F2C45dc832732aA188F78AA4f" BON/WMATIC
-        taxW = _taxW; // "0xb1a23cD1dcB4F07C9d766f2776CAa81d33fa0Ede" bonDevs
+        erc20contract = _erc20contract; // "0x47E53f0Ddf71210F2C45dc832732aA188F78AA4f" BON
+        erc20LP = _erc20LP; // "0x26432f7cf51e644c0adcaf3574216ee1c0a9af6d" BON/WMATIC
         erc20Base = _erc20Base; // "750000000000000000000" 750 tokens ~$4
         erc20Fee = _erc20Fee; // "1000" 10% of erc20 price (1,000/10,000)
         counter = 1;
@@ -48,16 +49,21 @@ contract Q2E {
         uint256 price = currentPrice();
 
         // all USERS must APPROVE lotto contract to use erc20 before v-this-v can work
-        bool paymentTxn = IERC20(erc20contract).transferFrom(msg.sender, address(this), price);
-        require(paymentTxn == true, "transfer failed!");
+        require(IERC20(erc20contract).transferFrom(msg.sender, address(this), price), "transfer failed!");
 
-        uint256 tax = price * erc20Fee / 10000;
-        uint256 payment = price - tax;
+        // tax token swap payment process
+        uint256 amountIn = price * erc20Fee / 10000;
+        address[] memory path = new address[](2);
+        path[0] = erc20LP.token0();
+        path[1] = erc20LP.token1();
+        oracleData[] = erc20LP.getReserves();
+        amountOutMinBeforeSlip = (oracleData[0] / oracleData[1] * amountIn);
+        amountOutMin = amountOutMinBeforeSlip - (amountOutMinBeforeSlip * 30/1000);
+        // contract(this) must have APPROVEd uniswap to use 'token0' before v-this-v can work
+        require(UniswapV2Router02.swapExactTokensForETH(amountIn, amountOutMin, path, address(this), block.timestamp), "transfer failed!");
 
-        // DEVS must APPROVE uniswap to use contracts erc20 before v-this-v can work
-        bool taxTxn = IERC20(erc20contract).transferFrom(msg.sender, taxW, tax);
-        require(taxTxn == true, "transfer failed!");
 
+        /* --- quiz stuff which I can figure out later ---
         // emit the guess so that it can be recorded on the frontend (and help future guesses)
         emit AnswerGuessed(answer);
         emit QuizFunded(IERC20(erc20contract).balanceOf(msg.sender));
@@ -67,10 +73,15 @@ contract Q2E {
             payable(msg.sender).transfer(address(this).balance);
             emit AnswerCorrect(winnings);
         }
+        */
     }
 
-    function bonFundsConvert() public{
+    function closeLotto() external onlyOwner{
+        // transfer() WMATIC to msg.sender
+    }
 
+    function approveUni() external onlyOwner{
+        // maybe this APPROVAL is needed to let the LOTTO CONTRACT approve Uni to transfer tokens?
     }
 
     fallback() external payable{
